2a10b475b8f5c67c3c9bdae782af83480b54d8c3
boltaevt <boltaevt@gmail.com>
2023-07-23 11:13:44 +0200

    feature implementation

--- main-service/src/main/java/ru/practicum/main/request/repository/RequestsRepository.java
+++ main-service/src/main/java/ru/practicum/main/request/repository/RequestsRepository.java
@@ -3,10 +3,12 @@ package ru.practicum.main.request.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import ru.practicum.main.request.enums.RequestStatus;
 import ru.practicum.main.request.model.Request;
 import ru.practicum.main.request.model.RequestInfo;
 
 import java.util.Collection;
+import java.util.Optional;
 
 @Repository
 public interface RequestsRepository extends JpaRepository<Request, Long> {
@@ -17,6 +19,8 @@ public interface RequestsRepository extends JpaRepository<Request, Long> {
     Collection<Request> findAllByEventId(Long eventId);
 
     Collection<Request> findAllByIdIn(Collection<Long> ids);
+
+    Optional<Request> findByRequesterIdAndEventIdAndStatus(Long userId, Long eventId, RequestStatus status);
 
     @Query("select new ru.practicum.main.request.model.RequestInfo(req.event.id, count(req.id)) " +
             "from Request as req " +
