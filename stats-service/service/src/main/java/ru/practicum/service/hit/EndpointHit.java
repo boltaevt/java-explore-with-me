d2e2276b31389e466eac97e8d82c40e83728751e
boltaevt <76846248+boltaevt@users.noreply.github.com>
2023-08-30 17:11:36 +0500

    Update EndpointHit.java

--- stats-service/service/src/main/java/ru/practicum/service/hit/EndpointHit.java
+++ stats-service/service/src/main/java/ru/practicum/service/hit/EndpointHit.java
@@ -18,7 +18,7 @@ public class EndpointHit {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "APP_ID")
     private App app;
 
